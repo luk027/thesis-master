22aa4a169ec5ee463fc8f3e0b8d6b55e1ccc4c75
Luk027 <lookman.gohil@gmail.com>
2024-09-10 19:36:34 +0530

    add:google-translate & fix:tablet-responsive

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+
 let year = document.getElementById("showYear");
 const d = new Date();
 
 let currentYear = d.getFullYear();
-year.innerHTML = currentYear+",";
+year.innerHTML = currentYear+" - ";
+
 

c3bd78e79489dfd72afb237dcf0ecb11febb4478
Luk027 <lookman.gohil@gmail.com>
2024-09-12 00:01:21 +0530

    add:scroll

--- src/script.js
+++ src/script.js
@@ -1,11 +1,33 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+function hexadeciToDeci(hex) {
+    return parseInt(hex, 16);
 }
+const data2 = "00000002245992EE";
+const decimalVal = hexadeciToDeci(data2);
+
+const Data = decimalVal.toString().concat("31");
+function encryptData(Data) {
+    return btoa(Data);
+}
+function decryptData(encryptData) {
+    return atob(encryptData);
+}
+const encryptedData = encryptData(Data);
+const decryptedData = decryptData(encryptedData);
+const waLink = `https://wa.me/${decryptedData}`;
+
+
+
+const contactBtn = document.getElementById('contact-btn');
+contactBtn.addEventListener('click', function () {
+    window.location.href = waLink;
+});
+
+
 
 let year = document.getElementById("showYear");
-const d = new Date();
+const date = new Date();
+let currentYear = date.getFullYear();
+year.innerHTML = currentYear + " - ";
 
-let currentYear = d.getFullYear();
-year.innerHTML = currentYear+" - ";
 
 
